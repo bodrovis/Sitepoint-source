a5594023f44590879ce5e4e7b4ea77bbb6b44ca1
bodrovis <ibodrov@campaigner.ru>
2015-03-03 18:52:02 +0300

    password strength

--- Authentication_with_Devise/app/models/user.rb
+++ Authentication_with_Devise/app/models/user.rb
@@ -29,7 +29,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable, :zxcvbnable
 
   def send_devise_notification(notification, *args)
     devise_mailer.send(notification, self, *args).deliver_later unless Rails.env.production?
--- Authentication_with_Devise/config/initializers/devise.rb
+++ Authentication_with_Devise/config/initializers/devise.rb
@@ -1,6 +1,8 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  config.min_password_score = 4
+
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
