2f201be01f8c26d6efa0d53014b04820fa4b1fd8
bodrovis <ibodrov@campaigner.ru>
2015-06-20 17:01:40 +0300

    minor updates

--- SimpleUploader/README.md
+++ SimpleUploader/README.md
@@ -2,4 +2,4 @@ Source code for the []() article on
 SitePoint,
 created by Ilya Bodrov ([radiant-wind.com](http://radiant-wind.com)).
 
-[Working demo](http://sitepoint-poller.herokuapp.com/).
+[Working demo](https://sitepoint-paperclip-uploader.herokuapp.com/).
--- SimpleUploader/app/models/photo.rb
+++ SimpleUploader/app/models/photo.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: photos
+#
+#  id                 :integer          not null, primary key
+#  title              :string
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  image_file_name    :string
+#  image_content_type :string
+#  image_file_size    :integer
+#  image_updated_at   :datetime
+#
+
 class Photo < ActiveRecord::Base
   has_attached_file :image,
                     #url: "/system/:hash.:extension",
