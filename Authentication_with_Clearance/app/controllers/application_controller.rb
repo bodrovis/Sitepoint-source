f673465a99bdbcad1917cf77c8bc452cf8e8395d
bodrovis <ibodrov@campaigner.ru>
2015-08-27 18:11:08 +0300

    controller

--- Authentication_with_Clearance/app/controllers/application_controller.rb
+++ Authentication_with_Clearance/app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def url_after_denied_access_when_signed_out
-    '/'
-  end
+  # def url_after_denied_access_when_signed_out
+  #   '/'
+  #end
 end
