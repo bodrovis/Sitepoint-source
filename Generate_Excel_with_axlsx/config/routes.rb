96dd1044c4eb7d8daa0f04082c6287e6a480b014
bodrovis <ibodrov@campaigner.ru>
2016-03-28 17:51:39 +0300

    axlsx

---
+++ Generate_Excel_with_axlsx/config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resources :products, only: [:index, :show]
+
+  root 'products#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Serve websocket cable requests in-process
+  # mount ActionCable.server => '/cable'
+end
---
+++ Generate_Excel_with_axlsx/db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+30.times do
+  Product.create({title: Faker::Commerce.product_name, price: Faker::Commerce.price})
+end
