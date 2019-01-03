e7e107fc3727581024e9e154f88e52b0b68d5f8b
vinoth241 <vinothcse41@gmail.com>
2019-01-03 17:01:02 +0530

    update helper file

--- Dota_on_Rails/app/helpers/application_helper.rb
+++ Dota_on_Rails/app/helpers/application_helper.rb
@@ -2,4 +2,7 @@ module ApplicationHelper
   def page_header(text)
     content_for(:page_header) { text.to_s.html_safe }
   end
+   def page_header_new(text)
+    content_for(:page_header) { text.to_s.html_safe }
+  end
 end
